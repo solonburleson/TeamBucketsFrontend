eb69f03904552a84a4dbeeffdbe81b4eb431e53b
Quoc Truong <quoctruong@utexas.edu>
2020-01-22 20:44:29 -0600

    fixed minor bugs

--- bankingAngular/src/app/user.service.ts
+++ bankingAngular/src/app/user.service.ts
@@ -9,11 +9,9 @@ import User from './user';
 })
 export class UserService {
   uri: string;
-  roleUri: string;
 
   constructor(private http: HttpClient) {
     this.uri = 'http://localhost:8080/api/users';
-    this.roleUri = 'http://localhost:8080/api/users/name';
   }
 
   getUsers(): Observable<User[]> {
@@ -21,6 +19,6 @@ export class UserService {
   }
   
   getUserByName(name): Observable<User> {
-    return this.http.get<User>(`${this.roleUri}/${name}`);
+    return this.http.get<User>(`${this.uri}/${name}`);
   }
 }
